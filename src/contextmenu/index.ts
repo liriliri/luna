1abfb124ad487d572ae9a4069e5afe15a999f70c
surunzi <redhoodsu@tencent.com>
2020-08-26 21:26:28 +0800

    chore: small changes

--- src/contextmenu/index.ts
+++ src/contextmenu/index.ts
@@ -1,6 +1,25 @@
 import './style.scss'
+import $ from 'licia/$'
 
 module.exports = class Contextmenu {
-  constructor() {}
-  show() {}
+  private $container: $.$
+  private menuItems: MenuItem[] = []
+  constructor(container: Element) {
+    this.$container = $(container)
+  }
+  append(options: IMenuItemOptions) {
+    this.menuItems.push(new MenuItem(options))
+  }
+  popup(x: number, y: number) {
+    this.popupMenuItems(this.menuItems, x, y)
+  }
+  private popupMenuItems(menuItems: MenuItem[], x: number, y: number) {}
+}
+
+interface IMenuItemOptions {
+  label: string
+}
+
+class MenuItem {
+  constructor(options: IMenuItemOptions) {}
 }

19ee5ca7dbada38b73c3451268bf332722a9273a
surunzi <surunzi@foxmail.com>
2020-12-26 15:56:11 +0800

    fix: test

--- src/music-player/test.js
+++ src/music-player/test.js
@@ -5,7 +5,7 @@ require('./icon.css')
 describe('music-player', function () {
   it('basic', function () {
     const container = document.getElementById('container')
-    const musicPlayer = new LunaMusicPlayer(container, {
+    const musicPlayer = new MusicPlayer(container, {
       audio: [],
     })
     musicPlayer.play()

72d3284fb2d568347dbe7ed7a3b9c36706fbb3d2
surunzi <surunzi@foxmail.com>
2021-01-03 13:55:34 +0800

    feat: editor

--- src/editor/index.ts
+++ src/editor/index.ts
@@ -23,16 +23,33 @@ export = class Editor extends Component {
   }
   private bindEvent() {
     const { c } = this
-    this.$toolbar.on('click', c('.bold'), this.onBoldClick)
+    this.$toolbar
+      .on('click', c('.bold'), this.onBoldClick)
+      .on('click', c('.italic'), this.onItalicClick)
+      .on('click', c('.underline'), this.onUnderlineClick)
+      .on('click', c('.strike-through'), this.onStrikeThroughClick)
+      .on('click', c('.quote'), this.onQuoteClick)
+      .on('click', c('.header'), this.onHeaderClick)
+      .on('click', c('.horizontal-rule'), this.onHorizontalRuleClick)
   }
-  private onBoldClick = () => {
-    document.execCommand('bold')
+  private onBoldClick = () => this.exec('bold')
+  private onItalicClick = () => this.exec('italic')
+  private onUnderlineClick = () => this.exec('underline')
+  private onStrikeThroughClick = () => this.exec('strikeThrough')
+  private onQuoteClick = () => this.exec('formatBlock', '<blockquote>')
+  private onHeaderClick = () => this.exec('formatBlock', '<h1>')
+  private onHorizontalRuleClick = () => this.exec('insertHorizontalRule')
+  private exec(command: string, val?: string) {
+    document.execCommand(command, false, val)
     this.content.focus()
   }
   private appendTpl() {
     this.$container.html(
       this.c(stripIndent`
       <div class="toolbar">
+        <button class="button header">
+          <span class="icon icon-header"></span>
+        </button>
         <button class="button bold">
           <span class="icon icon-bold"></span>
         </button>
@@ -42,8 +59,14 @@ export = class Editor extends Component {
         <button class="button underline">
           <span class="icon icon-underline"></span>
         </button>
-        <button class="button line-through">
-          <span class="icon icon-line-through"></span>
+        <button class="button strike-through">
+          <span class="icon icon-strike-through"></span>
+        </button>
+        <button class="button quote">
+          <span class="icon icon-quote"></span>
+        </button>
+        <button class="button horizontal-rule">
+          <span class="icon icon-horizontal-rule"></span>
         </button>
       </div>  
       <div class="body">

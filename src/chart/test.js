6696874f52f70f5de5dc4712385d817cb815a4bb
surunzi <redhoodsu@tencent.com>
2021-01-28 17:04:29 +0800

    chore: small changes

--- src/chart/test.js
+++ src/chart/test.js
@@ -5,25 +5,31 @@ document.body.appendChild(canvas)
 canvas.width = 600
 canvas.width = 300
 
-new Chart(canvas, {
-  type: 'bar',
-  bgColor: '#fbfbfb',
-  title: {
-    text: 'Bar Chart',
-  },
-  data: {
-    labels: ['Monday', 'TuesDay', 'Wednesday', 'Thursday', 'Friday'],
-    datasets: [
-      {
-        label: 'Dataset 1',
-        bgColor: '#e73c5e',
-        data: [128, 146, 56, 84, 222],
+describe('chart', function () {
+  it('bar', function () {
+    const chart = new Chart(canvas, {
+      type: 'bar',
+      bgColor: '#fbfbfb',
+      title: {
+        text: 'Bar Chart',
       },
-      {
-        label: '#614d82',
-        bgColor: '#614d82',
-        data: [119, 23, 98, 67, 88],
+      data: {
+        labels: ['Monday', 'TuesDay', 'Wednesday', 'Thursday', 'Friday'],
+        datasets: [
+          {
+            label: 'Dataset 1',
+            bgColor: '#e73c5e',
+            data: [128, 146, 56, 84, 222],
+          },
+          {
+            label: '#614d82',
+            bgColor: '#614d82',
+            data: [119, 23, 98, 67, 88],
+          },
+        ],
       },
-    ],
-  },
+    })
+
+    expect(chart).to.be.an('object')
+  })
 })

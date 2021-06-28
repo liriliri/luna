9d870437592a2cbda20b4388a181a056f1577c0e
redhoodsu <redhoodsu@tencent.com>
2021-06-28 22:22:31 +0800

    chore(carousel): small changes

--- src/carousel/index.ts
+++ src/carousel/index.ts
@@ -2,26 +2,47 @@ import isStr from 'licia/isStr'
 import toEl from 'licia/toEl'
 import stripIndent from 'licia/stripIndent'
 import Component from '../share/Component'
+import $ from 'licia/$'
+import toArr from 'licia/toArr'
+import h from 'licia/h'
 
 export default class Carousel extends Component {
-  private slides: HTMLElement[] = []
+  private $body: $.$
+  private body: HTMLElement
   constructor(container: HTMLElement) {
     super(container, { compName: 'carousel' })
 
     this.initTpl()
+
+    this.$body = this.find('.body')
+    this.body = this.$body.get(0) as HTMLElement
   }
   append(content: string | HTMLElement) {
-    this.insert(this.slides.length, content)
+    const slides = this.getSlides()
+    this.insert(slides.length, content)
   }
   insert(pos: number, content: string | HTMLElement) {
+    const item = h(this.c('.item'))
     if (isStr(content)) {
       content = toEl(content as string) as HTMLElement
     }
-    this.slides.splice(pos, 0, content as HTMLElement)
+    item.appendChild(content as HTMLElement)
+    const slides = this.getSlides()
+    const len = slides.length
+    if (pos > len - 1) {
+      this.body.appendChild(item)
+    } else {
+      const nextEl = slides[pos]
+      this.body.insertBefore(item, nextEl)
+    }
+  }
+  private getSlides() {
+    return toArr(this.body.children)
   }
   private initTpl() {
     this.$container.html(
       this.c(stripIndent`
+      <div class="body"></body>
       <div class="arrow-left">
         <span class="icon icon-arrow-left"></span>
       </div>

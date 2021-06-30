d5e5c1c4a0cec5bb8b0eccc5012fd642966bd909
redhoodsu <redhoodsu@tencent.com>
2021-06-30 14:45:34 +0800

    feat(carousel): indicators

--- src/carousel/index.ts
+++ src/carousel/index.ts
@@ -6,13 +6,16 @@ import $ from 'licia/$'
 import toArr from 'licia/toArr'
 import h from 'licia/h'
 import idxOf from 'licia/idxOf'
+import toNum from 'licia/toNum'
 import { executeAfterTransition } from '../share/util'
 
 export default class Carousel extends Component {
   private $body: $.$
   private $arrowLeft: $.$
   private $arrowRight: $.$
+  private $indicators: $.$
   private body: HTMLElement
+  private activeIdx = 0
   constructor(container: HTMLElement) {
     super(container, { compName: 'carousel' })
 
@@ -22,8 +25,23 @@ export default class Carousel extends Component {
     this.body = this.$body.get(0) as HTMLElement
     this.$arrowLeft = this.find('.arrow-left')
     this.$arrowRight = this.find('.arrow-right')
+    this.$indicators = this.find('.indicators').find('ul')
 
     this.bindEvent()
+  }
+  slideTo(idx: number) {
+    const slides = this.getSlides()
+    const { activeIdx } = this
+
+    if (idx >= slides.length || idx < 0) {
+      return
+    }
+
+    if (activeIdx === idx) {
+      return
+    }
+
+    this.slide(idx > activeIdx ? 'next' : 'prev', slides[idx])
   }
   append(content: string | HTMLElement) {
     const slides = this.getSlides()
@@ -45,6 +63,8 @@ export default class Carousel extends Component {
       const nextEl = slides[pos]
       this.body.insertBefore(item, nextEl)
     }
+
+    this.updateIndicators()
   }
   next = () => {
     this.slide('next')
@@ -52,7 +72,7 @@ export default class Carousel extends Component {
   prev = () => {
     this.slide('prev')
   }
-  private slide(order: string) {
+  private slide(order: string, el?: HTMLElement) {
     const isNext = order === 'next'
     const slides = this.getSlides()
     const len = slides.length
@@ -60,13 +80,23 @@ export default class Carousel extends Component {
     const $activeEl = this.find('.active')
     const activeEl = $activeEl.get(0) as HTMLElement
     const activeIdx = idxOf(slides, activeEl)
-    let nextIdx = isNext ? activeIdx + 1 : activeIdx - 1
-    if (nextIdx >= len) {
-      nextIdx = 0
-    } else if (nextIdx < 0) {
-      nextIdx = len - 1
+
+    let nextEl: HTMLElement
+    let nextIdx = 0
+
+    if (el) {
+      nextEl = el
+      nextIdx = idxOf(slides, nextEl)
+    } else {
+      nextIdx = isNext ? activeIdx + 1 : activeIdx - 1
+      if (nextIdx >= len) {
+        nextIdx = 0
+      } else if (nextIdx < 0) {
+        nextIdx = len - 1
+      }
+      nextEl = slides[nextIdx]
     }
-    const nextEl = slides[nextIdx]
+
     const $nextEl = $(nextEl)
 
     const directionClass = this.c(isNext ? 'item-start' : 'item-end')
@@ -86,6 +116,23 @@ export default class Carousel extends Component {
       $activeEl.rmClass(directionClass)
       $activeEl.rmClass(activeClass)
     })
+
+    this.activeIdx = nextIdx
+    this.updateIndicators()
+  }
+  private updateIndicators() {
+    const { c, activeIdx } = this
+    const slides = this.getSlides()
+    const len = slides.length
+
+    let html = ''
+    for (let i = 0; i < len; i++) {
+      html += `<li class="${c(
+        'indicator ' + (activeIdx === i ? 'active' : '')
+      )}" data-idx="${i}"></li>`
+    }
+
+    this.$indicators.html(html)
   }
   private getSlides() {
     return toArr(this.body.children)
@@ -100,12 +147,20 @@ export default class Carousel extends Component {
       <div class="arrow-right">
         <span class="icon icon-arrow-right"></span>
       </div>
+      <div class="indicators"><ul></ul></div>
     `)
     )
   }
   private bindEvent() {
     this.$arrowLeft.on('click', this.prev)
     this.$arrowRight.on('click', this.next)
+
+    const self = this
+    this.$indicators.on('click', 'li', function (this: any) {
+      const $this = $(this)
+      const idx = toNum($this.data('idx'))
+      self.slideTo(idx)
+    })
   }
 }
 

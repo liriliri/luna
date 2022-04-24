acaef2411c4ca8408dd01a0fa9f5538bb1f4441d
redhoodsu <redhoodsu@tencent.com>
2022-04-24 19:53:31 +0800

    chore: theme color adjust

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -4,10 +4,7 @@ export default create({
   base: 'light',
   brandUrl: 'https://github.com/liriliri/luna',
   brandTitle: 'LUNA UI',
-  colorSecondary: '#E73C5E',
+  colorSecondary: '#1a73e8',
   appBg: '#F7F7F7',
-  appBorderColor: '#2D253C',
-  barTextColor: '#FFFFFF',
-  barBg: '#614D82',
   appContentBg: '#FFF',
 })

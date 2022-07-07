fbf540f3c5140c6c099ca14022803785bbfe514d
redhoodsu <redhoodsu@tencent.com>
2022-07-07 19:51:32 +0800

    feat(keyboard): responsive

--- src/keyboard/index.ts
+++ src/keyboard/index.ts
@@ -1,5 +1,8 @@
 import Component, { IComponentOptions } from '../share/Component'
 import stripIndent from 'licia/stripIndent'
+import { drag } from '../share/util'
+import $ from 'licia/$'
+import toNum from 'licia/toNum'
 
 /** IOptions */
 export interface IOptions extends IComponentOptions {}
@@ -8,17 +11,23 @@ export interface IOptions extends IComponentOptions {}
  * Virtual keyboard.
  */
 export default class Keyboard extends Component<IOptions> {
+  private input = ''
   constructor(container: HTMLElement, options: IOptions) {
     super(container, { compName: 'keyboard' })
 
     this.initOptions(options, {})
 
     this.initTpl()
+
+    this.bindEvent()
+  }
+  setInput(input: string) {
+    this.input = input
   }
   private initTpl() {
     this.$container.html(
       this.c(stripIndent`
-      <ul class="keys">
+      <ul class="row">
         <li data-key="27"><span>esc</span></li>
         <li data-key="112"><span>F1</span></li>
         <li data-key="113"><span>F2</span></li>
@@ -32,7 +41,8 @@ export default class Keyboard extends Component<IOptions> {
         <li data-key="121"><span>F10</span></li>
         <li data-key="122"><span>F11</span></li>
         <li data-key="123"><span>F12</span></li>
-        <li data-key="-1"><span>〇</span></li>
+      </ul>
+      <ul class="row">
         <li data-key="192"><span>~</span><span>\`</span></li>
         <li data-key="49"><span>!</span><span>1</span></li>
         <li data-key="50"><span>@</span><span>2</span></li>
@@ -46,8 +56,10 @@ export default class Keyboard extends Component<IOptions> {
         <li data-key="48"><span>)</span><span>0</span></li>
         <li data-key="189"><span>＿</span><span>-</span></li>
         <li data-key="187"><span>＋</span><span>: </span></li>
-        <li data-key="8"><span>delete</span></li>
-        <li data-key="9"><span>tab</span></li>
+        <li data-key="8"><span>&nbsp;</span><span>delete</span></li>
+      </ul>
+      <ul class="row">
+        <li data-key="9"><span>&nbsp;</span><span>tab</span></li>
         <li data-key="81"><span>Q</span></li>
         <li data-key="87"><span>W</span></li>
         <li data-key="69"><span>E</span></li>
@@ -61,7 +73,9 @@ export default class Keyboard extends Component<IOptions> {
         <li data-key="219"><span>{</span><span>[</span></li>
         <li data-key="221"><span>}</span><span>]</span></li>
         <li data-key="220"><span>|</span><span>\</span></li>
-        <li data-key="20"><span></span><span>CapsLock</span></li>
+      </ul>
+      <ul class="row">
+        <li data-key="20"><span>&nbsp;</span><span>CapsLock</span></li>
         <li data-key="65"><span>A</span></li>
         <li data-key="83"><span>S</span></li>
         <li data-key="68"><span>D</span></li>
@@ -74,7 +88,9 @@ export default class Keyboard extends Component<IOptions> {
         <li data-key="186"><span>:</span><span>;</span></li>
         <li data-key="222"><span>"</span><span>'</span></li>
         <li data-key="13"><span>enter</span><span>return</span></li>
-        <li data-key="16"><span>⇧</span></li>
+      </ul>
+      <ul class="row">
+        <li data-key="16"><span>&nbsp;</span><span>⇧</span></li>
         <li data-key="90"><span>Z</span></li>
         <li data-key="88"><span>X</span></li>
         <li data-key="67"><span>C</span></li>
@@ -85,21 +101,38 @@ export default class Keyboard extends Component<IOptions> {
         <li data-key="188"><span>&lt;</span><span>,</span></li>
         <li data-key="190"><span>&gt;</span><span>.</span></li>
         <li data-key="191"><span>?</span><span>/</span></li>
-        <li data-key="16"><span>⇧</span></li>
-        <li data-key="-1"><span>fn</span></li>
-        <li data-key="17"><span>control</span></li>
+        <li data-key="16"><span>&nbsp;</span><span>⇧</span></li>
+      </ul>
+      <ul class="row">
+        <li data-key="17"><span>&nbsp;</span><span>control</span></li>
+        <li data-key="91"><span>&nbsp;</span><span>command</span></li>
         <li data-key="18"><span>alt</span><span>option</span></li>
-        <li data-key="91"><span>command</span></li>
         <li data-key="32"><span></span></li>
-        <li data-key="93"><span>command</span></li>
         <li data-key="18"><span>alt</span><span>option</span></li>
+        <li data-key="93"><span>&nbsp;</span><span>command</span></li>
         <li data-key="37"><span>◀</span></li>
-        <li data-key="38"><span>▲</span></li>
+        <li>
+          <ul>
+            <li data-key="38"><span>▲</span></li>
+            <li data-key="40"><span>▼</span></li>
+          </ul>
+        </li>
         <li data-key="39"><span>▶</span></li>
-        <li data-key="40"><span>▼</span></li>
       </ul>
       `)
     )
+  }
+  private bindEvent() {
+    const self = this
+    this.find('.row').on(drag('start'), 'li', function (this: HTMLLIElement) {
+      const $li = $(this)
+      if (!$li.data('key')) {
+        return
+      }
+      const key = toNum($li.data('key'))
+      self.input += String.fromCharCode(key)
+      self.emit('change', self.input)
+    })
   }
 }
 
--- src/keyboard/story.js
+++ src/keyboard/story.js
@@ -2,11 +2,46 @@ import 'luna-keyboard.css'
 import Keyboard from 'luna-keyboard.js'
 import readme from './README.md'
 import story from '../share/story'
+import h from 'licia/h'
+import $ from 'licia/$'
 
 const def = story(
   'keyboard',
-  (container) => {
-    const keyboard = new Keyboard(container)
+  (wrapper) => {
+    $(wrapper).html('').css({
+      maxWidth: 800,
+      margin: '0 auto',
+      fontSize: 0,
+    })
+
+    const textarea = h('textarea', {
+      placeholder: 'Tap to start',
+    })
+    $(textarea).css({
+      width: '100%',
+      background: '#2e2e2e',
+      border: 'none',
+      boxSizing: 'border-box',
+      outline: 'none',
+      borderRadius: '4px 4px 0 0',
+      height: 134,
+      fontSize: '18px',
+      padding: 20,
+      resize: 'vertical',
+      color: '#fff',
+    })
+    wrapper.appendChild(textarea)
+
+    const keyboardContainer = h('div')
+    wrapper.appendChild(keyboardContainer)
+
+    const keyboard = new Keyboard(keyboardContainer)
+    keyboard.on('change', (input) => {
+      textarea.value = input
+    })
+    textarea.addEventListener('input', (event) => {
+      keyboard.setInput(event.target.value)
+    })
 
     return keyboard
   },

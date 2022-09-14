bf66eacb8a52da8ccc82e7dcb591c6ff48cfa7a8
redhoodsu <redhoodsu@tencent.com>
2022-09-14 19:03:27 +0800

    chore: small changes

--- src/color-picker/index.ts
+++ src/color-picker/index.ts
@@ -2,6 +2,10 @@ import $ from 'licia/$'
 import Component, { IComponentOptions } from '../share/Component'
 import stripIndent from 'licia/stripIndent'
 import Color from 'licia/Color'
+import { rgbToHsv } from './util'
+import { drag } from '../share/util'
+
+const $document = $(document as any)
 
 /** IOptions */
 export interface IOptions extends IComponentOptions {
@@ -18,6 +22,7 @@ export interface IOptions extends IComponentOptions {
 export default class ColorPicker extends Component<IOptions> {
   private color: Color
   private $saturation: $.$
+  private $saturationPointer: $.$
   constructor(container: HTMLElement, options: IOptions = {}) {
     super(container, { compName: 'color-picker' })
     this.$container = $(container)
@@ -30,14 +35,49 @@ export default class ColorPicker extends Component<IOptions> {
 
     this.initTpl()
     this.$saturation = this.find('.saturation')
+    this.$saturationPointer = this.find('.saturation-pointer')
+
+    this.bindEvent()
 
     this.updateColor()
   }
   private updateColor() {
     const hsl = this.color.toHsl()
     const color = Color.parse(hsl)
+    const hsv = this.getHsv()
 
     this.$saturation.css('background', `hsl(${color.val[0]},100%, 50%)`)
+    this.updateSaturationPointer(-hsv[2] + 100, hsv[1])
+  }
+  private getHsv() {
+    const rgb = this.color.toRgb()
+    const color = Color.parse(rgb)
+    const { val } = color
+
+    return rgbToHsv(val[0], val[1], val[2])
+  }
+  private bindEvent() {
+    const { c } = this
+    this.$saturation.on(
+      drag('start'),
+      c('.saturation-pointer'),
+      this.onSaturationStart
+    )
+  }
+  private onSaturationStart = () => {
+    $document.on(drag('move'), this.onSaturationMove)
+    $document.on(drag('end'), this.onSaturationEnd)
+  }
+  private onSaturationMove = (e: any) => {}
+  private updateSaturationPointer(top: number, left: number) {
+    this.$saturationPointer.css({
+      top: `${top}%`,
+      left: `${left}%`,
+    })
+  }
+  private onSaturationEnd = () => {
+    $document.off(drag('move'), this.onSaturationMove)
+    $document.off(drag('end'), this.onSaturationEnd)
   }
   private initTpl() {
     this.$container.html(
---
+++ src/color-picker/util.ts
@@ -0,0 +1,26 @@
+const round = Math.round
+export function rgbToHsv(r: number, g: number, b: number) {
+  let h = 0
+  const max = Math.max(r, g, b)
+  const min = Math.min(r, g, b)
+  const delta = max - min
+
+  if (delta === 0) {
+    h = 0
+  } else if (r === max) {
+    h = ((g - b) / delta) % 6
+  } else if (g === max) {
+    h = (b - r) / delta + 2
+  } else if (b === max) {
+    h = (r - g) / delta + 4
+  }
+
+  h = round(h * 60)
+  if (h < 0) h += 360
+
+  const s = round((max === 0 ? 0 : delta / max) * 100)
+
+  const v = round((max / 255) * 100)
+
+  return [h, s, v]
+}

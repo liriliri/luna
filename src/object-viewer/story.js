811483e37dc3be386e9c2ede1b6b1c89b1700097
redhoodsu <redhoodsu@tencent.com>
2022-11-08 17:15:55 +0800

    chore: small changes

--- src/object-viewer/story.js
+++ src/object-viewer/story.js
@@ -1,5 +1,6 @@
 import 'luna-object-viewer.css'
-import ObjectViewer from 'luna-object-viewer.js'
+import ObjectViewer, { Static } from 'luna-object-viewer.js'
+import stringifyAll from 'licia/stringifyAll'
 import readme from './README.md'
 import { text, boolean } from '@storybook/addon-knobs'
 import story from '../share/story'
@@ -8,18 +9,30 @@ const def = story(
   'object-viewer',
   (container) => {
     const target = text('Target', 'navigator')
+    const useStatic = boolean('Use Static Object Viewer', false)
     const unenumerable = boolean('Show Unenumerable', true)
     const accessGetter = boolean('Access Getter', true)
 
     if (window[target]) {
-      const objectViewer = new ObjectViewer(container, {
-        unenumerable,
-        accessGetter,
-      })
+      if (useStatic) {
+        const data = stringifyAll(window[target], {
+          unenumerable,
+          accessGetter,
+        })
+        const staticObjectViewer = new Static(container)
+        staticObjectViewer.set(data)
 
-      objectViewer.set(window[target])
+        return staticObjectViewer
+      } else {
+        const objectViewer = new ObjectViewer(container, {
+          unenumerable,
+          accessGetter,
+        })
 
-      return objectViewer
+        objectViewer.set(window[target])
+
+        return objectViewer
+      }
     }
   },
   {

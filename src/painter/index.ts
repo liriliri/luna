d421d1a6f14395745284f218e3c4659ba8d85d7f
redhoodsu <redhoodsu@tencent.com>
2024-01-02 19:58:32 +0800

    chore: small changes

--- src/painter/index.ts
+++ src/painter/index.ts
@@ -1,7 +1,12 @@
 import Component, { IComponentOptions } from '../share/Component'
+import stripIndent from 'licia/stripIndent'
+import $ from 'licia/$'
 
+/** IOptions. */
 export interface IOptions extends IComponentOptions {
+  /** Canvas width. */
   width?: number
+  /** Canvas height. */
   height?: number
 }
 
@@ -13,9 +18,32 @@ export interface IOptions extends IComponentOptions {
  * const painer = new LunaPainter(container)
  */
 export default class Painter extends Component<IOptions> {
-  constructor(container: HTMLElement, options: IOptions) {
-    super(container, { compName: 'painer' }, options)
+  private $viewport: $.$
+  constructor(container: HTMLElement, options: IOptions = {}) {
+    super(container, { compName: 'painter' }, options)
 
-    this.initOptions(options, {})
+    this.initOptions(options, {
+      width: 800,
+      height: 600,
+    })
+
+    this.initTpl()
+
+    this.$viewport = this.find('.viewport')
+  }
+  private initTpl() {
+    const { width, height } = this.options
+
+    this.$container.html(
+      this.c(stripIndent`
+        <div class="viewport">
+          <div class="canvas-wrapper">
+            <div class="canvas-container">
+              <canvas width="${width}" height="${height}"></canvas>
+            </div>
+          </div>
+        </div>
+      `)
+    )
   }
 }
--- src/painter/story.js
+++ src/painter/story.js
@@ -1,12 +1,23 @@
-import 'luna-painter'
+import 'luna-painter.css'
 import Painter from 'luna-painter.js'
 import story from '../share/story'
 import readme from './README.md'
+import $ from 'licia/$'
 
 const def = story(
   'painter',
   (container) => {
-    const painter = new Painter(container)
+    $(container).css({
+      width: '100%',
+      maxWidth: 1200,
+      height: 600,
+      margin: '0 auto',
+    })
+
+    const painter = new Painter(container, {
+      width: 512,
+      height: 512,
+    })
 
     return painter
   },
